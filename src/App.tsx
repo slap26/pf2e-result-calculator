92f227756686d1af8794346f4ca10325cd881314
jferraro <140004014+JFerraro26@users.noreply.github.com>
2025-04-09 14:58:05 -0700

    more basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import { BrowserRouter } from "react-router-dom";
 import Calculator from "./calculator";
-
 function App() {
 	return (
 		<BrowserRouter>
-			<div className="min-h-screen min-w-full bg-zinc-800 font-sans text-orange-600">
-				<Calculator />
-			</div>
+			<Calculator />
 		</BrowserRouter>
 	);
 }
--- src/calculator.tsx
+++ src/calculator.tsx
@@ -1,372 +1,57 @@
-import Strike from "./actions/strike";
-import { act, useEffect, useState } from "react";
-import { MonsterLevelType, MonsterDifficultyType } from "./tables";
+import { useState } from "react";
 
 function Calculator() {
-	const [playerLvl, setPlayerLvl] = useState<number>(1);
-	const [strength, setStrength] = useState<number>(0);
-	const [dexterity, setDexterity] = useState<number>(0);
-	// const [constitution, setConstitution] = useState<number>(0);
-	// const [intelligence, setIntelligence] = useState<number>(0);
-	// const [charisma, setCharisma] = useState<number>(0);
-	// const [wisdom, setWisdom] = useState<number>(0);
-	const [action, setAction] = useState<string>("strike");
-	const [monsterLvl, setMonsterLvl] = useState<MonsterLevelType>("-1");
-	const [monsterDifficulty, setMonsterDifficulty] =
-		useState<MonsterDifficultyType>("moderate");
-	const [playerStatusBonus, setPlayerStatusBonus] = useState<number>(0);
-	const [playerCircumstanceBonus, setPlayerCircumstanceBonus] =
-		useState<number>(0);
-	const [playerBonus, setPlayerBonus] = useState<number>(0);
-	const [monsterStatusBonus, setMonsterStatusBonus] = useState<number>(0);
-	const [monsterCircumstanceBonus, setMonsterCircumstanceBonus] =
-		useState<number>(0);
-	const [monsterBonus, setMonsterBonus] = useState<number>(0);
-	const monsterLvls = Array.from({ length: 26 }, (_, i) => ({
-		value: i - 1,
-		label: `${i - 1}`,
-	}));
-
-	const playerLvls = Array.from({ length: 20 }, (_, index) => ({
-		value: index + 1,
-		label: `${index + 1}`,
-	}));
-
-	useEffect(() => {
-		setPlayerBonus(playerStatusBonus + playerCircumstanceBonus);
-	}, [playerStatusBonus, playerCircumstanceBonus]);
-
-	useEffect(() => {
-		setMonsterBonus(monsterStatusBonus + monsterCircumstanceBonus);
-	}, [monsterCircumstanceBonus, monsterStatusBonus]);
-
 	return (
-		<main className="flex flex-col flex-grow min-h-screen">
-			<div className="flex flex-col justify-evenly items-center w-full mt-10">
-				<h1 className="text-violet-700 text-2xl font-bold">
-					Player Character
-				</h1>
-				<div className="flex flex-row place-content-evenly w-full p-1">
-					<div className="flex flex-col place-items-center">
-						<h2>Lvl</h2>
-						<select
-							id="playerLvl"
-							value={playerLvl}
-							onChange={(e) =>
-								setPlayerLvl(parseInt(e.target.value))
-							}
-							className="bg-slate-700 rounded-md"
-						>
-							{playerLvls.map((plvl) => (
-								<option key={plvl.value} value={plvl.value}>
-									{plvl.label}
-								</option>
-							))}
-						</select>
-					</div>
-					<div className="flex flex-col place-items-center">
-						<h2>STR</h2>
-						<select
-							id="strength"
-							value={strength}
-							onChange={(e) =>
-								setStrength(parseInt(e.target.value))
-							}
-							className="bg-slate-700 rounded-md"
-						>
-							<option value={-4}>-4</option>
-							<option value={-3}>-3</option>
-							<option value={-2}>-2</option>
-							<option value={-1}>-1</option>
-							<option value={0}>0</option>
-							<option value={1}>1</option>
-							<option value={2}>2</option>
-							<option value={3}>3</option>
-							<option value={4}>4</option>
-							<option value={5}>5</option>
-							<option value={6}>6</option>
-						</select>
-					</div>
-					<div className="flex flex-col place-items-center">
-						<h2>DEX</h2>
-						<select
-							id="dexterity"
-							value={dexterity}
-							onChange={(e) =>
-								setDexterity(parseInt(e.target.value))
-							}
-							className="bg-slate-700 rounded-md"
-						>
-							<option value={-4}>-4</option>
-							<option value={-3}>-3</option>
-							<option value={-2}>-2</option>
-							<option value={-1}>-1</option>
-							<option value={0}>0</option>
-							<option value={1}>1</option>
-							<option value={2}>2</option>
-							<option value={3}>3</option>
-							<option value={4}>4</option>
-							<option value={5}>5</option>
-							<option value={6}>6</option>
-						</select>
-					</div>
-					{/* <div className="flex flex-col place-items-center">
-						<h2>CON</h2>
-						<select
-							id="constitution"
-							value={constitution}
-							onChange={(e) =>
-								setConstitution(parseInt(e.target.value))
-							}
-							className="bg-slate-700 rounded-md"
-						>
-							<option value={-4}>-4</option>
-							<option value={-3}>-3</option>
-							<option value={-2}>-2</option>
-							<option value={-1}>-1</option>
-							<option value={0}>0</option>
-							<option value={1}>1</option>
-							<option value={2}>2</option>
-							<option value={3}>3</option>
-							<option value={4}>4</option>
-							<option value={5}>5</option>
-							<option value={6}>6</option>
-						</select>
-					</div>
-					<div className="flex flex-col place-items-center">
-						<h2>INT</h2>
-						<select
-							id="intelligence"
-							value={intelligence}
-							onChange={(e) =>
-								setIntelligence(parseInt(e.target.value))
-							}
-							className="bg-slate-700 rounded-md"
-						>
-							<option value={-4}>-4</option>
-							<option value={-3}>-3</option>
-							<option value={-2}>-2</option>
-							<option value={-1}>-1</option>
-							<option value={0}>0</option>
-							<option value={1}>1</option>
-							<option value={2}>2</option>
-							<option value={3}>3</option>
-							<option value={4}>4</option>
-							<option value={5}>5</option>
-							<option value={6}>6</option>
-						</select>
-					</div>
-					<div className="flex flex-col place-items-center">
-						<h2>WIS</h2>
-						<select
-							id="wisdom"
-							value={wisdom}
-							onChange={(e) =>
-								setWisdom(parseInt(e.target.value))
-							}
-							className="bg-slate-700 rounded-md"
-						>
-							<option value={-4}>-4</option>
-							<option value={-3}>-3</option>
-							<option value={-2}>-2</option>
-							<option value={-1}>-1</option>
-							<option value={0}>0</option>
-							<option value={1}>1</option>
-							<option value={2}>2</option>
-							<option value={3}>3</option>
-							<option value={4}>4</option>
-							<option value={5}>5</option>
-							<option value={6}>6</option>
-						</select>
-					</div>
-					<div className="flex flex-col place-items-center">
-						<h2>CHA</h2>
-						<select
-							id="charisma"
-							value={charisma}
-							onChange={(e) =>
-								setCharisma(parseInt(e.target.value))
-							}
-							className="bg-slate-700 rounded-md"
-						>
-							<option value={-4}>-4</option>
-							<option value={-3}>-3</option>
-							<option value={-2}>-2</option>
-							<option value={-1}>-1</option>
-							<option value={0}>0</option>
-							<option value={1}>1</option>
-							<option value={2}>2</option>
-							<option value={3}>3</option>
-							<option value={4}>4</option>
-							<option value={5}>5</option>
-							<option value={6}>6</option>
-						</select>
-					</div> */}
+		<div className="flex flex-col justify-around items-center h-screen w-full font-sans p-8 bg-gray-400">
+			<div className="flex flex-col items-center w-full border p-2">
+				<div className="flex flex-col justify-center items-center w-full text-violet-600">
+					<h1 className="text-3xl">PF2E AC/DC</h1>
+					<h1 className="text-3xl">Calculator</h1>
 				</div>
-				<div className="flex flex-row">
-					<div className="flex flex-col">
-						<h2>Circumstance Bonus</h2>
-						<select
-							id="playerCircumstanceBonus"
-							value={playerCircumstanceBonus}
-							onChange={(e) =>
-								setPlayerCircumstanceBonus(
-									parseInt(e.target.value)
-								)
-							}
-							className="bg-slate-700 rounded-md"
-						>
-							<option value={-4}>-4</option>
-							<option value={-3}>-3</option>
-							<option value={-2}>-2</option>
-							<option value={-1}>-1</option>
-							<option value={0}>0</option>
-							<option value={1}>1</option>
-							<option value={2}>2</option>
-							<option value={3}>3</option>
-							<option value={4}>4</option>
-						</select>
+				<div className="flex flex-row justify-around w-full">
+					<div className="flex flex-col items-center">
+						<p>Modifier</p>
+						<input type="number" className="border max-w-24" />
 					</div>
-					<div className="flex flex-col">
-						<h2>Status Bonus</h2>
-						<select
-							id="playerStatusBonus"
-							value={playerStatusBonus}
-							onChange={(e) =>
-								setPlayerStatusBonus(parseInt(e.target.value))
-							}
-							className="bg-slate-700 rounded-md"
-						>
-							<option value={-4}>-4</option>
-							<option value={-3}>-3</option>
-							<option value={-2}>-2</option>
-							<option value={-1}>-1</option>
-							<option value={0}>0</option>
-							<option value={1}>1</option>
-							<option value={2}>2</option>
-							<option value={3}>3</option>
-							<option value={4}>4</option>
-						</select>
+					<div className="flex flex-col items-center">
+						<p>AC/DC</p>
+						<input type="number" className="border max-w-24" />
+					</div>
+				</div>
+				<div className="flex flex-row justify-around w-full">
+					<div className="flex flex-row">
+						<p>Agile?</p>
+						<input type="checkbox" />
+					</div>
+					<button className="border">=</button>
+				</div>
+				<div className="flex flex-col w-full">
+					<div className="flex flex-row justify-around w-full">
+						<button className="border">1st</button>
+						<button className="border">2nd</button>
+						<button className="border">3rd</button>
+					</div>
+					<div className="flex flex-row justify-around">
+						<div className="flex flex-col items-center justify-center text-red-600">
+							<p className="text-center">Crit Fail</p>
+							<p className="text-center">5%</p>
+						</div>
+						<div className="flex flex-col items-center justify-center text-red-600">
+							<p className="text-center">Fail</p>
+							<p className="text-center">10%</p>
+						</div>
+						<div className="flex flex-col items-center justify-center text-green-600">
+							<p className="text-center">Success</p>
+							<p className="text-center">70%</p>
+						</div>
+						<div className="flex flex-col items-center justify-center text-green-600">
+							<p className="text-center">Crit Success</p>
+							<p className="text-center">15%</p>
+						</div>
 					</div>
 				</div>
 			</div>
-			<div className="flex flex-col justify-evenly items-center w-full mt-6">
-				<h1 className="text-violet-700 text-2xl font-bold">Monster</h1>
-				<div className="flex flex-row w-full justify-evenly p-1">
-					<div className="flex flex-col place-items-center">
-						<h2>Lvl</h2>
-						<select
-							className="bg-slate-700 rounded-md"
-							id="monsterLvl"
-							value={monsterLvl}
-							onChange={(e) =>
-								setMonsterLvl(
-									e.target.value as MonsterLevelType
-								)
-							}
-						>
-							{monsterLvls.map((monsterLvl) => (
-								<option
-									key={monsterLvl.value}
-									value={monsterLvl.value}
-								>
-									{monsterLvl.label}
-								</option>
-							))}
-						</select>
-					</div>
-					<div className="flex flex-col place-items-center">
-						<h2>Difficulty</h2>
-						<select
-							className="bg-slate-700 rounded-md"
-							id="monsterDifficulty"
-							value={monsterDifficulty}
-							onChange={(e) =>
-								setMonsterDifficulty(
-									e.target.value as MonsterDifficultyType
-								)
-							}
-						>
-							<option value={"low"}>Low</option>
-							<option value={"moderate"}>Moderate</option>
-							<option value={"high"}>High</option>
-							<option value={"extreme"}>Extreme</option>
-						</select>
-					</div>
-				</div>
-				<div className="flex flex-row">
-					<div className="flex flex-col">
-						<h2>Circumstance Bonus</h2>
-						<select
-							id="monsterCircumstanceBonus"
-							value={monsterCircumstanceBonus}
-							onChange={(e) =>
-								setMonsterCircumstanceBonus(
-									parseInt(e.target.value)
-								)
-							}
-							className="bg-slate-700 rounded-md"
-						>
-							<option value={-4}>-4</option>
-							<option value={-3}>-3</option>
-							<option value={-2}>-2</option>
-							<option value={-1}>-1</option>
-							<option value={0}>0</option>
-							<option value={1}>1</option>
-							<option value={2}>2</option>
-							<option value={3}>3</option>
-							<option value={4}>4</option>
-						</select>
-					</div>
-					<div className="flex flex-col">
-						<h2>Status Bonus</h2>
-						<select
-							id="monsterStatusBonus"
-							value={monsterStatusBonus}
-							onChange={(e) =>
-								setMonsterStatusBonus(parseInt(e.target.value))
-							}
-							className="bg-slate-700 rounded-md"
-						>
-							<option value={-4}>-4</option>
-							<option value={-3}>-3</option>
-							<option value={-2}>-2</option>
-							<option value={-1}>-1</option>
-							<option value={0}>0</option>
-							<option value={1}>1</option>
-							<option value={2}>2</option>
-							<option value={3}>3</option>
-							<option value={4}>4</option>
-						</select>
-					</div>
-				</div>
-			</div>
-			<div className="flex flex-col place-items-center mt-6">
-				<h2 className="text-violet-700 text-2xl font-bold">Action</h2>
-				<select
-					className=" bg-slate-700 rounded-md p-1"
-					id="action"
-					value={action}
-					onChange={(e) => setAction(e.target.value)}
-				>
-					<option value={"strike"}>Strike</option>
-				</select>
-			</div>
-
-			{action === "strike" ? (
-				<Strike
-					monsterLvl={monsterLvl}
-					monsterDifficulty={monsterDifficulty}
-					playerLvl={playerLvl}
-					strength={strength}
-					dexterity={dexterity}
-					playerBonus={playerBonus}
-					monsterBonus={monsterBonus}
-				/>
-			) : (
-				<p>Placeholder</p>
-			)}
-		</main>
+		</div>
 	);
 }
 
