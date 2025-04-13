1d2b6a81e04e2c7e57da8a21c37443a9c775888c
jferraro <140004014+JFerraro26@users.noreply.github.com>
2025-04-13 14:13:48 -0700

    working on a fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-	base: "/pf2e-result-calculator/",
+	base: `/pf2e-result-calculator/`,
 	plugins: [react()],
 });
